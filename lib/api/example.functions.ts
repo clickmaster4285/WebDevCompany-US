5f9a928a739e32651cda9161ec77bbcbcb5a7278
maybeadeel <adeelshah491@gmail.com>
2026-07-01 12:19:14 +0500

    delete some files

--- lib/api/example.functions.ts
+++
@@ -1,9 +0,0 @@
-import { getServerConfig } from "../config.server";
-
-export async function getGreeting(name: string) {
-  const config = getServerConfig();
-  return {
-    greeting: `Hello, ${name}!`,
-    mode: config.nodeEnv ?? "unknown",
-  };
-}
