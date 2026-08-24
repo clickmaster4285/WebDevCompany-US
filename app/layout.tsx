e0946515714d177120aabd57b250b1fbf33a209c
khan321-dev <ihtishamkhan.123bc@gmail.com>
2026-08-24 15:57:01 +0500

    complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from "next";
-import Script from "next/script"; // <-- Added this import
+import Script from "next/script";
 import { Geist } from "next/font/google";
 import { siteConfig } from "@/lib/siteConfig";
 import { GlobalSpotlight } from "@/components/GlobalSpotlight";
@@ -84,6 +84,17 @@ export default function RootLayout({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-NGB935HHG7');
+          `}
+        </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "x7vbb2jt0z");
           `}
         </Script>
 
